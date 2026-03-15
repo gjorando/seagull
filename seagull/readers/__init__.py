501327ecb36331f3b8e1dc7ce704beff221739f3
gjorando <guillaume.jorandon.1@ulaval.ca>
2026-03-15 11:15:44 -0400

    Removed unused code

--- seagull/readers/__init__.py
+++ seagull/readers/__init__.py
@@ -1,6 +1,5 @@
-from seagull.readers import metadata_processors
 from seagull.readers.reader import Reader
 from seagull.readers.rst_reader import RstReader
 from seagull.readers.static_reader import StaticReader
 
-__all__ = ["Reader", "RstReader", "StaticReader", "metadata_processors"]
+__all__ = ["Reader", "RstReader", "StaticReader"]
